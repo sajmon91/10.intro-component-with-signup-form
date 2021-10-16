a61facf73a9afd1bac843b783e9f6e68a67057f1
Stefan Simonovic <77784883+sajmon91@users.noreply.github.com>
2021-10-16 13:09:31 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Frontend Mentor - Intro component with sign-up form
+
+![Design preview for the Intro component with sign-up form](./images/desktop-preview.jpg)
+
+### Links
+
+- Live Site URL: [Link](https://adoring-joliot-e76055.netlify.app/)
---
+++ app.js
@@ -0,0 +1,64 @@
+const freeTrialForm = document.querySelector('form');
+const inputs = [...freeTrialForm.querySelectorAll('input')];
+
+
+const getFormattedInputName = function (name) {
+	return name.split('-').map(str => str[0].toUpperCase() + str.slice(1)).join(' ');
+};
+
+const getErrorMessage = function (input) {
+	const inputName = input.getAttribute('name');
+	const inputNameFormatted = getFormattedInputName(inputName);
+
+	if (input.validity.valueMissing) {
+		return `${inputNameFormatted} cannot be empty`;
+	}
+	if (input.validity.tooLong) {
+		return `${inputNameFormatted} is too long`;
+	}
+	if (input.validity.tooShort) {
+		return `${inputNameFormatted} should have at least ${input.getAttribute(
+			'minlength'
+		)} characters`;
+	}
+	if (inputName === 'email' && input.validity.patternMismatch) {
+		return 'Looks like this is not an email';
+	}
+
+	return `${inputNameFormatted} is invalid`;
+};
+
+const validateInput = function (input) {
+	const errorContainer = input.parentElement.querySelector('.error');
+
+	if (!input.validity.valid) {
+		errorContainer.textContent = getErrorMessage(input);
+		errorContainer.classList.remove('hidden');
+		input.classList.add('input--invalid');
+		input.classList.remove('input--valid');
+		return false;
+	}
+
+	errorContainer.textContent = '';
+	errorContainer.classList.add('hidden');
+	input.classList.remove('input--invalid');
+	input.classList.add('input--valid');
+	return true;
+};
+
+freeTrialForm.addEventListener('input', function (e) {
+	const input = e.target.closest('.input');
+	if (!input) return;
+
+	validateInput(input);
+});
+
+freeTrialForm.addEventListener('submit', function (e) {
+	let formInvalid = false;
+	for (const input of inputs) {
+		if (!validateInput(input)) formInvalid = true;
+	}
+	if (formInvalid) e.preventDefault();
+});
+
+freeTrialForm.noValidate = true;
